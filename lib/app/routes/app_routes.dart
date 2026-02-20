bc5f54e676806671d345784f2ffa6f40cde5de7d
Avinash175 <avinashmagar15@gmail.com>
2026-02-20 13:34:17 +0530

    initialize new Flutter project with multi-platform setup

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,15 @@
+part of 'app_pages.dart';
+
+abstract class Routes {
+  Routes._();
+  static const MANAGE_SERVICES = '/manage-services';
+  static const ADD_SERVICE = '/add-service';
+  static const BOOKING_CALENDAR = '/booking-calendar';
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const MANAGE_SERVICES = '/manage-services';
+  static const ADD_SERVICE = '/add-service';
+  static const BOOKING_CALENDAR = '/booking-calendar';
+}
---
+++ lib/app/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryStatusColor = Color(0xFF1F0B46);
+  static const Color backgroundColor = Color(0xFFF6F6F6);
+  static const Color accentColor = Color(0xFFE26F40);
+  static const Color buttonColor = Color(0xFF9879E3);
+  static const Color cardColor = Colors.white;
+  static const Color textDark = Color(0xFF333333);
+  static const Color textLight = Color(0xFF8C8C8C);
+}
